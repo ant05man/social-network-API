5e2a63c092b3e3bc53e7319cfd6803a56f463bb3
Anthony Littlejohn <ant05man@yahoo.com>
2023-11-04 18:25:09 -0400

    added index for routes folder

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+// Import all API routes from /api/index.js 
+const apiRoutes = require('./api');
+
+// add prefix of `/api` to all of the api routes imported from the `api` directory
+router.use('/api', apiRoutes);
+
+router.use((req, res) => res.send('Wrong route!'));
+
+module.exports = router;
