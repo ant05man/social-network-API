5cf5199099f3b86742f95ab0e90068820bbec212
Anthony Littlejohn <ant05man@yahoo.com>
2023-11-04 17:11:21 -0400

    created reaction schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,35 @@
 const { Schema, model, types } = require('mongoose');
 const moment = require('moment')
 
+// reaction schema
+const reactionSchema = new Schema (
+    {
+        reactionId: {
+           type: Schema.Types.ObjectId, 
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
+        },
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false,
+    })
 
 
 // thought Schema
