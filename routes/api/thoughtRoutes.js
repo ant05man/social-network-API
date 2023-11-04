875643b65111b18f67862e58d5cd8d5fb4409cb7
Anthony Littlejohn <ant05man@yahoo.com>
2023-11-04 18:53:24 -0400

    created thoughtRoutes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+
+const {
+    getThought,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    deleteReaction
+} = require('../../controllers/thoughtController');
+
+// /api/thoughts GET all and POST thought
+router.route('/').get(getThought).post(createThought);
+
+// /api/thoughts/:thoughtId GET one thought, PUT and DELETE by iD
+router.route('/:thoughtId')
+.get(getSingleThought)
+.put(updateThought)
+.delete(deleteThought);
+
+//  /api/thoughts/:thoughtId/reactions POST new reactions
+router.route('/:thoughtId/reactions')
+.post(createReaction);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId DELETE reaction by ID
+router.route('/:thoughtId/reactions/:reactionId')
+.delete(deleteReaction);
+
+
+module.exports = router;
